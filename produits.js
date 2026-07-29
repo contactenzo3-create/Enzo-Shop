b4c5c8df8ffc761af69963df38bd66f140abc822
contactenzo3-create <contactenzo3@gmail.com>
2026-07-29 15:47:48 +0200

    Add files via upload

---
+++ produits.js
@@ -0,0 +1,49 @@
+﻿// =================================
+// ENZO SHOP - LISTE DES PRODUITS
+// =================================
+
+const produits = [
+
+    // 👕 OCCASION
+
+    // 🎮 GAMING
+
+    {
+
+        id: 5,
+
+        nom: "🎮 Manette PS4 Hogwarts Legacy - Portkey Games",
+
+        description: "Manette en bon état pour jouer sur PS4.",
+
+        prix: 35,
+
+        categorie: "Gaming",
+
+        image: "images/gaming/manette1.jpg"
+
+    },
+
+    // 💻 HIGH-TECH
+
+    // 📦 COLLECTION
+
+    {
+        id: 9,
+        nom: "Funko Pop Batman #1189",
+        description: "Figurine Funko Pop Batman #1189 jamais sortie de sa boîte. État collection.",
+        prix: 25,
+        categorie: "Collection",
+        image: "images/collection/batman1189.jpg"
+    },
+
+    {
+        id: 10,
+        nom: "Funko Pop Groot #416",
+        description: "Figurine Funko Pop Groot #416 avec sa boîte. Déjà ouverte mais conservée en très bon état.",
+        prix: 15,
+        categorie: "Collection",
+        image: "images/collection/groot416.jpg"
+    }
+
+];
